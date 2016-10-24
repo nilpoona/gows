c85acd93723a8a11d890b733300609a96b04f184
YutakaHorikawa <gozutakemaru@gmail.com>
2016-10-24 00:29:06 -0700

    update: remove origin check

--- server/client.go
+++ server/client.go
@@ -31,6 +31,7 @@ var (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 // Client is an middleman between the websocket connection and the hub.
