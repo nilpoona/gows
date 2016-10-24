20f7b8abeb57099ac9fea3966fdde3eae036c755
YutakaHorikawa <gozutakemaru@gmail.com>
2016-10-24 00:29:24 -0700

    update: dev client

---
+++ client/index.js
@@ -0,0 +1,42 @@
+var msg = null;
+var log = null;
+
+function appendLog(item) {
+    var doScroll = log.scrollTop === log.scrollHeight - log.clientHeight;
+    log.appendChild(item);
+    if (doScroll) {
+        log.scrollTop = log.scrollHeight - log.clientHeight;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    msg = document.getElementById("msg");
+    log = document.getElementById("log");
+
+    document.getElementById("form").onsubmit = function () {
+        if (!conn) {
+            return false;
+        }
+        if (!msg.value) {
+            return false;
+        }
+        conn.send(msg.value);
+        msg.value = "";
+        return false;
+    };
+
+    const conn = new WebSocket("ws://127.0.0.1:8088/room/1");
+    conn.onclose = function (evt) {
+        let item = document.createElement("div");
+        item.innerHTML = "<b>Connection closed.</b>";
+        appendLog(item);
+    };
+    conn.onmessage = function (evt) {
+        var messages = evt.data.split('\n');
+        for (var i = 0; i < messages.length; i++) {
+            var item = document.createElement("div");
+            item.innerText = messages[i];
+            appendLog(item);
+        }
+    };
+});
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+var destPath = 'dest';
+
+let config = {
+    entry: ['./client/index.js'],
+    output: {
+        path: path.resolve(destPath),
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel',
+                exclude: /node_modules/,
+            },
+            { 
+                test: /\.json$/, loader: "json-loader"
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            }
+        })
+    ],
+    devServer: {
+        contentBase: 'dest',
+        port: 8091,
+        host: "0.0.0.0",
+        inline: true,
+    }
+};
+
+module.exports = config
