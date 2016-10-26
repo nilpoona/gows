07d12398f72642de3d57f0f58b7f180b0eb13c31
YutakaHorikawa <gozutakemaru@gmail.com>
2016-10-26 01:43:07 -0700

    update: listne unix domain socket

--- server/main.go
+++ server/main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gorilla/mux"
 )
 
 var addr = flag.String("addr", ":8088", "http service address")
+
+func shutdown(listener net.Listener) {
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		if err := listener.Close(); err != nil {
+			log.Printf("error: %v", err)
+		}
+		os.Exit(1)
+
+	}()
+}
 
 func main() {
 	flag.Parse()
@@ -19,8 +36,25 @@ func main() {
 		vars := mux.Vars(r)
 		serveWs(hub, w, r, vars["id"])
 	})
-	err := http.ListenAndServe(*addr, r)
+
+	listener, err := net.Listen("unix", "/tmp/ws.sock")
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+		log.Fatalf("error: %v", err)
 	}
+
+	defer func() {
+		if err := listener.Close(); err != nil {
+			log.Printf("error: %v", err)
+		}
+	}()
+
+	if err := http.Serve(listener, r); err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	/*
+		err := http.ListenAndServe(*addr, r)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
+		}
+	*/
 }
